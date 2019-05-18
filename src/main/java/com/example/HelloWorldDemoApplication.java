f019eff040cbb1f4c3e01a5df2f00aac902b9ef2
eddy <eddy.lxj@gmail.com>
2019-05-18 14:57:18 -0400

    add hello world

--- src/main/java/com/example/HelloWorldDemoApplication.java
+++ src/main/java/com/example/HelloWorldDemoApplication.java
@@ -1,12 +1,16 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by Eddy on 5/18/19
  */
+
+@SpringBootApplication
 public class HelloWorldDemoApplication {
     public static void main(String[] args){
         SpringApplication.run(HelloWorldDemoApplication.class);
+
     }
 }
