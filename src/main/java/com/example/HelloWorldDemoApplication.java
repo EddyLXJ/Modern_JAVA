a9dee637b28908654177cba06b89b1f58880632c
eddy <eddy.lxj@gmail.com>
2019-05-18 14:40:21 -0400

    init

---
+++ src/main/java/com/example/HelloWorldDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by Eddy on 5/18/19
+ */
+public class HelloWorldDemoApplication {
+    public static void main(String[] args){
+        SpringApplication.run(HelloWorldDemoApplication.class);
+    }
+}
