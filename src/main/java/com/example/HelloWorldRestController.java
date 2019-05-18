954c9af8a655943d4eacf87b9e3f4f4629c1b46c
eddy <eddy.lxj@gmail.com>
2019-05-18 15:01:56 -0400

    add path name

--- src/main/java/com/example/HelloWorldRestController.java
+++ src/main/java/com/example/HelloWorldRestController.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldRestController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String greeting(){
-        return "Hello Spring Boot";
+    @RequestMapping(value = "/{studentName}", method = RequestMethod.GET)
+    public String greeting(@PathVariable String studentName){
+        return "Hello Spring Boot" + studentName;
     }
+
+
 }
